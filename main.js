9456fc9053485d034a41dcd6055231020daa7317
Alabhya Jindal <alabhya10@gmail.com>
2023-05-10 02:46:16 +0530

    300 nodes

--- main.js
+++ main.js
@@ -5,3 +5,16 @@ const app = new PIXI.Application({
 });
 
 canvas.appendChild(app.view);
+
+for (let i = 0; i < 300; i++) {
+  const graphy = new PIXI.Graphics();
+  graphy.beginFill(0xffffff);
+  graphy.lineStyle(2, 0x111111);
+  graphy.drawRect(0, 0, 100, 45);
+  graphy.endFill();
+
+  app.stage.addChild(graphy);
+
+  graphy.x = Math.floor(Math.random() * 600) + 20;
+  graphy.y = Math.floor(Math.random() * 500) + 10;
+}
